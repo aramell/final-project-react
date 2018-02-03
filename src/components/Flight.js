e267a541fd3c1a6e74bfb651edb77aee7b8f4064
Andrew <andrew.ramell@gmail.com>
2018-02-03 15:28:13 -0500

    added flight container

--- src/components/Flight.js
+++ src/components/Flight.js
@@ -1,10 +1,11 @@
 import React, {component} from 'react'
-
+import FlightContainer from './FlightContainer'
 
  
 const Flight = ({date, time, planeNumber, flightTime}) => {
   return(
     <div>
+      <h2> Future Flights </h2>
       <p> {date} </p>
        <p> {time} </p>
        <p> {planeNumber}</p>
---
+++ src/components/FlightContainer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Flight from './Flight'
+
+class FlightContainer extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+      
+    }
+  }
+  render (){
+    return (
+      <div>
+        <Flight  />
+      </div>
+    )
+  }
+}
+export default FlightContainer
