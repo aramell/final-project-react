07170d06a1b4a341ebd503828f5b3463f65a046c
Andrew <andrew.ramell@gmail.com>
2018-02-03 22:24:20 -0500

    added rails api

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 import flightReducer from './reducers/flightReducer'
 import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(flightReducer)
 
-ReactDOM.render(<App store={store}/>, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App store={store}/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
 
