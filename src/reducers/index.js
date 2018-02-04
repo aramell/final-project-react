88260e533a7b581edb9371d1def63de59414510f
Andrew <andrew.ramell@gmail.com>
2018-02-04 15:14:53 -0500

    added rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,2 +1,9 @@
 import {combineReducers} from 'redux'
-import { routerReducer} from 'redux'
+// import { routerReducer} from 'react-router-redux'
+
+import flightReducer from './flightReducer'
+import planeReducer from './planeReducer'
+
+const rootReducer = combineReducers({flightReducer, planeReducer})
+
+export default rootReducer
