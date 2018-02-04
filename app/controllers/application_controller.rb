89a206fdd57f329bad5509c8998cd15242511520
Andrew <andrew.ramell@gmail.com>
2018-02-03 22:35:24 -0500

    added application controller rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+
+  def index 
+    @flights = Flight.all
+  end
+
+  def new 
+    @flight = Flight.new
+  end
+
+  def create
+    @flight = Flight.create(flight_params)
+  end
+
+  def show
+    @flight = Flight.find(params[:id])
+
+  end
+
+  def edit
+    @flight = Flight.find(id: )
+  end
+
+  private
+  def flight_params
+    params.require(:flight).permit(:date, :planenumber, :time, :flighttime)
+  end
 end
