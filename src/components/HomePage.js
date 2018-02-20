89a3c53b7af14a8180b493f820c999cfa63957b2
Andrew <andrew.ramell@gmail.com>
2018-02-20 18:26:58 -0500

    added picture and bootstrap

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,15 +4,25 @@ import WeatherContainer from './WeatherContainer';
 import CreateFlight from './CreateFlight';
 import FlightIndex from './FlightIndex';
 import FlightPage from './FlightPage';
+import  '../images/planephoto.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import TextBox from './Textbox'
 // import Calendar from '../containers/Calendar'
 
 class HomePage extends React.Component{
+ 
 
   render(){
+    const planeStyle = {
+      top: 0,
+      right: 0,
+      height: '50%',
+      width: '50%',
+    }
     return(
       <div className="header">
         <div className="title">
+          <img src={require(`../images/planephoto.jpg`)} id="plane" style={planeStyle}/>
           <h1>Flight Scheduler </h1>
           </div>
           <div className="weather-box">
